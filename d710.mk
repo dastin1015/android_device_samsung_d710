4e6b2a0fb22788682d9c0eec2faf416027075094
Dorian Snyder <dastin1015@gmail.com>
2013-08-10 13:28:00 -0700

    d710: remove DeviceSettings

--- d710.mk
+++ d710.mk
@@ -54,10 +54,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/configs/sirfgps.conf:system/etc/sirfgps.conf
 
-# Packages
-PRODUCT_PACKAGES := \
-    DeviceSettings
-
 # WIMAX
 PRODUCT_PACKAGES += \
     AngryGPS \
